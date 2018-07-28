8b8bf1b0d40f755a0a0abd8a7863c9deda4815e7
samarv <samarvir1996@gmail.com>
2018-07-28 11:30:21 +0530

    desgined thinking

--- App.js
+++ App.js
@@ -1,40 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View,Button , TextInput} from 'react-native';
+import { StyleSheet,Alert, Text, View,Button , TextInput} from 'react-native';
 import { Constants } from 'expo'
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
     paddingTop: Constants.statusBarHeight,
     borderRadius: 4,
     borderWidth: 1,
     borderColor: 'red',
   },
-  timeContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  textTimeContainer: {
-    fontSize: 32
-  },
-  inputContainer: {
+  inputMinutes: {
+    height: 50,
     borderRadius: 4,
     borderWidth: 1,
-    borderColor: 'red',
+    borderColor: 'black',
+    margin:10,
+  },
+  buttonMinute: {
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: 'black',
     padding:10
   }
 });
-
-
-onPauseButton = () => {
-  console.log("pause pressed8")
-}
-
-onStopButton = () => {
-  console.log("pause pressed8")
-}
-
 
 
 
@@ -53,123 +43,38 @@ export default class App extends React.Component {
     };
 
   }
-
-  
-  // incrementCount = () => {
-  //   this.setState(prevState => ({count: prevState.count + 1}))
-  //   if(this.state.count % 60 === 0 && this.state.count !== 0){
-  //     this.incrementMinute();
-  //   }
-  //   if(this.state.min % 60 === 0 &&  this.state.min !== 0){
-  //     this.incrementHour();
-  //   }
-
-  //   if(this.state.hour % 24 === 0 &&  this.state.hour !== 0){
-  //     this.incrementDay();
-  //   }
-  // }
-
-  // incrementMinute = () => {
-  //   this.setState(prevState => ({
-  //     count: 0,
-  //     min: prevState.min + 1
-  //   }))
-  // }
-
-  // incrementHour = () => {
-  //   this.setState(prevState => ({
-  //     min: 0,
-  //     hour: prevState.hour + 1
-  //   }))
-  // }
-
-  // incrementDay = () => {
-  //   this.setState(prevState => ({
-  //     hour: 0
-  //   }))
-  // }
-
-  decrementStart = () => {
-    setInterval(this.decrementTimer, 1000)
+  onSubmit = async () => {
+    
+    var result = await this.setMinute()
+    setInterval(this.decrementCount, 1000)
+    
   }
 
-  decrementTimer = () => {
-    // if timer hour > 0 or timerminute> 0 or timersecond>0
-    //  decrease 1 from from timer second, 
-    //else
-    // do nothing
-    if (this.state.timerSecond > 0){
-      this.decrementSecond();
-    }
-    else if (this.state.timerSecond === 0) {
-      if(this.state.timerMinute > 0){
-        this.decrementMinute();
-      }
-      else if(this.state.timerMinute === 0 && this.state.timerHour > 0){
-        this.decrementHour();
-      }
-      else{
-        console.log(this.state.timerSecond)
-      }
-    }
-    else{
-      console.log(this.state.timerSecond)
-    }
+  setMinute = () => {
+    let secs = this.state.timerMinute * 60
+      console.log(secs)
+      this.setState({
+        timerSecond: secs
+      })
   }
 
-  decrementSecond = () => {
+  decrementCount = () => {
     this.setState(prevState => ({timerSecond: prevState.timerSecond - 1}))
   }
-
-  decrementMinute = () => {
-    this.setState(prevState => ({
-      timerSecond: 59,
-      timerMinute: prevState.timerMinute - 1
-    }))
-  }
-
-  decrementHour = () => {
-    this.setState(prevState => ({
-      timerMinute: 59,
-      timerHour: prevState.timerHour - 1
-    }))
-  }
-
-  
   render() {
     return (
     <View style={[styles.container]}>
-      <View style={[styles.timeContainer]}>
-        <Text style={[styles.textTimeContainer]}>{this.state.timerHour}</Text>
-        <Text style={[styles.textTimeContainer]}>:</Text>
-        <Text style={[styles.textTimeContainer]}>{this.state.timerMinute}</Text>
-        <Text style={[styles.textTimeContainer]}>:</Text>
-        <Text style={[styles.textTimeContainer]}>{this.state.timerSecond}</Text>
-      </View>
-      <View style={[styles.timeContainer]}>
-        <TextInput
-          style={[styles.inputContainer]}
-          placeholder="put in hours"
-          onChangeText={(timerHour) => this.setState({timerHour})}
+      <TextInput
+          style={[styles.inputMinutes]}
+          placeholder="type in number of minutes!"
+          onChangeText={(timerMinute) => this.setState({timerMinute})}
         />
-        <TextInput
-          style={[styles.inputContainer]}
-          placeholder="put in minutes"
-          onChangeText={(timerminute) => this.setState({timerminute})}
-        />
-        <TextInput
-          style={[styles.inputContainer]}
-          placeholder="put in seconds"
-          onChangeText={(timerSecond) => this.setState({timerSecond})}
-        />
-      </View>
-      <View style={[styles.timeContainer]}>
-        <Button
-              onPress = {this.decrementStart}
-              title="Start the timer"
-              color="#841584"
-            />
-      </View>
+      <Button 
+        onPress={this.onSubmit}
+        title="Go! Go! Go!"
+      />
+      <Text> { this.state.timerMinute} </Text>
+      <Text> { this.state.timerSecond} </Text>
     </View>
     );
   }
